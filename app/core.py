e22ef3d81d5e920822a0b1edbcf7aae780b9c832
Mike Karolow <mobilemike@gmail.com>
2024-11-17 15:43:00 -0500

    Initial commit

---
+++ app/core.py
@@ -0,0 +1,148 @@
+"""Core logic."""
+
+import uuid
+from datetime import datetime, timezone
+from typing import TYPE_CHECKING, Any, Callable, Literal, LiteralString
+
+import google.generativeai as genai
+import httpx
+import structlog
+from fastapi import Body, FastAPI, Request, Response
+from pydantic import Field, HttpUrl, SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from starlette.background import BackgroundTask
+from typing_extensions import Annotated
+from zoneinfo import ZoneInfo
+
+from app.models.bb.webhook import WebhookNewMessage, WebhookTypingIndicator
+
+if TYPE_CHECKING:
+    from google.generativeai.types.generation_types import GenerateContentResponse
+
+
+class Settings(BaseSettings):
+    """Settings class for managing environment variables and configuration."""
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+    google_ai_api_key: SecretStr = Field(validation_alias="GOOGLE_AI_PAID_API_KEY")
+    bb_url: HttpUrl
+    bb_password: SecretStr
+    environment: str
+
+
+settings = Settings()  # pyright: ignore [reportCallIssue]
+
+log: structlog.stdlib.BoundLogger = structlog.get_logger()
+app = FastAPI()
+genai.configure(api_key=settings.google_ai_api_key.get_secret_value())
+
+SYSTEM_PROMPT_TEMPLATE: LiteralString = """You are Alpha.
+
+The current date is {now}.
+
+Alpha is ungendered and is referred to as "it/them".
+
+Alpha communicates with others via iMessage, simmilar to texting. Responses should be concise and clear.
+
+Alpha is now being connected with a human. The human will be able to see the conversation and will be able to respond to you.
+""".strip()  # noqa: E501
+
+
+def log_info(req_body: bytes, res_body: bytes) -> None:
+    """Log the incoming and outgoing requests."""
+    log.debug("Request", body=req_body)
+    log.debug("Response", body=res_body)
+
+
+# @app.middleware("http")
+async def logging_middleware(request: Request, call_next: Callable) -> Response:
+    """Log incoming and outgoing requests."""
+    req_body: bytes = await request.body()
+    response: Any = await call_next(request)
+
+    res_body: Literal[b""] = b""
+    async for chunk in response.body_iterator:
+        res_body += chunk
+
+    task = BackgroundTask(log_info, req_body, res_body)
+    return Response(
+        content=res_body,
+        status_code=response.status_code,
+        headers=dict(response.headers),
+        media_type=response.media_type,
+        background=task,
+    )
+
+
+def system_prompt() -> str:
+    """Generate a system prompt."""
+    # format the system prompt with the current date and time
+    now: str = (
+        datetime.now(tz=timezone.utc)
+        .astimezone(ZoneInfo("America/New_York"))
+        .strftime("%Y-%m-%d %H:%M:%S")
+    )
+    return SYSTEM_PROMPT_TEMPLATE.format(now=now)
+
+
+def generate_reply(message: str) -> str:
+    """Send a reply to the user."""
+    model = genai.GenerativeModel(
+        "gemini-1.5-flash",
+        system_instruction=system_prompt(),
+    )
+    tools: dict[str, dict[str, dict[str, float]]] = {
+        "google_search_retrieval": {
+            "dynamic_retrieval_config": {"dynamic_threshold": 0.5},
+        },
+    }
+
+    gemini_response: GenerateContentResponse = model.generate_content(
+        message,
+        tools=tools,
+        safety_settings="BLOCK_NONE",
+    )
+
+    text: LiteralString = gemini_response.text.strip()
+    log.info("Generated text", text=text)
+    return text
+
+
+def send_message_to_bb(payload: WebhookNewMessage, message: str) -> None:
+    """Send a message to BlueBubbles."""
+    url: str = f"{settings.bb_url}/message/text"
+    params: dict[str, str] = {"password": settings.bb_password.get_secret_value()}
+    data: dict[str, str] = {
+        "method": "private-api",
+        "chatGuid": payload.data.chats[0].guid,
+        "tempGuid": str(uuid.uuid4()),
+        "message": message,
+    }
+    response: httpx.Response = httpx.post(url, params=params, json=data)
+    log.info("Message sent", response=response)
+
+
+@app.post("/webhook")
+async def post_webhook(
+    payload: Annotated[
+        WebhookNewMessage | WebhookTypingIndicator,
+        Body(
+            ...,
+            discriminator="type",
+        ),
+    ],
+) -> str:
+    """Incomming webhooks from BlueBubbles."""
+    # switch on the class of the payload object
+    if isinstance(payload, WebhookNewMessage):
+        log.info("New message", payload=payload)
+        if payload.data.is_from_me:
+            return "OK"
+        message: str = generate_reply(payload.data.text)
+        send_message_to_bb(payload, message)
+
+    elif isinstance(payload, WebhookTypingIndicator):
+        log.info("User is typing", payload=payload)
+
+    return "OK"
---
+++ app/models/bb/webhook.py
@@ -0,0 +1,103 @@
+"""Pydantic models for incoming webhooks from BB."""
+
+from datetime import datetime
+from typing import Literal
+
+from pydantic import BaseModel, Field
+
+
+class Handle(BaseModel):
+    """Handle model."""
+
+    original_rowid: int = Field(alias="originalROWID")
+    address: str
+    service: str
+    uncanonical_id: str | None = Field(alias="uncanonicalizedId")
+    country: str
+
+
+class Chat(BaseModel):
+    """Chat model."""
+
+    original_rowid: int = Field(alias="originalROWID")
+    guid: str
+    style: int
+    chat_identifier: str = Field(alias="chatIdentifier")
+    is_archived: bool = Field(alias="isArchived")
+    display_name: str = Field(alias="displayName")
+
+
+class AttachementMetadata(BaseModel):
+    """Attachment metadata model."""
+
+    size: int
+    height: int
+    width: int
+
+
+class Attachememt(BaseModel):
+    """Attachment model."""
+
+    original_rowid: int = Field(alias="originalROWID")
+    guid: str
+    uti: str
+    mime_type: str = Field(alias="mimeType")
+    transfer_name: str = Field(alias="transferName")
+    total_bytes: int = Field(alias="totalBytes")
+    height: int
+    width: int
+    metadata: AttachementMetadata
+
+
+class WebhookNewMessageData(BaseModel):
+    """Webhook data model."""
+
+    original_rowid: int = Field(alias="originalROWID")
+    guid: str
+    text: str
+    attributed_body: str | None = Field(alias="attributedBody")
+    handle: Handle
+    handle_id: int = Field(alias="handleId")
+    other_handle: int = Field(alias="otherHandle")
+    attachments: list[Attachememt]
+    subject: str | None
+    error: int
+    date_created: datetime = Field(alias="dateCreated")
+    date_read: datetime | None = Field(alias="dateRead")
+    date_delivered: datetime | None = Field(alias="dateDelivered")
+    is_from_me: bool = Field(alias="isFromMe")
+    has_dd_results: bool = Field(alias="hasDdResults")
+    is_archived: bool = Field(alias="isArchived")
+    item_type: int = Field(alias="itemType")
+    group_title: str | None = Field(alias="groupTitle")
+    group_action_type: int = Field(alias="groupActionType")
+    balloon_bundle_id: str | None = Field(alias="balloonBundleId")
+    associated_message_guid: str | None = Field(alias="associatedMessageGuid")
+    associated_message_type: str | None = Field(alias="associatedMessageType")
+    expressive_send_style_id: str | None = Field(alias="expressiveSendStyleId")
+    thread_originator_guid: str | None = Field(alias="threadOriginatorGuid")
+    has_payload_data: bool = Field(alias="hasPayloadData")
+    chats: list[Chat]
+    message_summary_info: dict | None = Field(alias="messageSummaryInfo")
+    payload_data: dict | None = Field(alias="payloadData")
+
+
+class WebhookNewMessage(BaseModel):
+    """Webhook model for new messages."""
+
+    type: Literal["new-message"]
+    data: WebhookNewMessageData
+
+
+class WebhookTypingIndicatorData(BaseModel):
+    """Webhook data model for typing indicators."""
+
+    display: bool
+    guid: str
+
+
+class WebhookTypingIndicator(BaseModel):
+    """Webhook model for typing indicators."""
+
+    type: Literal["typing-indicator"]
+    data: WebhookTypingIndicatorData
