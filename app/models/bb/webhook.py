adc303fc27ab4d9d33df5396bf6453107d417783
Mike Karolow <mobilemike@gmail.com>
2024-11-22 19:10:51 -0500

    chore: fix models

--- app/models/bb/webhook.py
+++ app/models/bb/webhook.py
@@ -4,32 +4,39 @@ from datetime import datetime
 from typing import Literal
 from uuid import UUID
 
-from pydantic import BaseModel, Field
+from humps import camelize
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class Handle(BaseModel):
     """Handle model."""
 
+    model_config = ConfigDict(alias_generator=camelize, populate_by_name=True)
+
     original_rowid: int = Field(alias="originalROWID")
     address: str
     service: str
-    uncanonical_id: str | None = Field(alias="uncanonicalizedId")
+    uncanonicalized_id: str | None
     country: str
 
 
 class Chat(BaseModel):
     """Chat model."""
 
+    model_config = ConfigDict(alias_generator=camelize, populate_by_name=True)
+
     original_rowid: int = Field(alias="originalROWID")
-    guid: UUID
+    guid: str
     style: int
-    chat_identifier: str = Field(alias="chatIdentifier")
-    is_archived: bool = Field(alias="isArchived")
-    display_name: str = Field(alias="displayName")
+    chat_identifier: str
+    is_archived: bool
+    display_name: str
 
 
 class AttachementMetadata(BaseModel):
     """Attachment metadata model."""
+
+    model_config = ConfigDict(alias_generator=camelize, populate_by_name=True)
 
     size: int
     height: int
@@ -39,59 +46,81 @@ class AttachementMetadata(BaseModel):
 class Attachememt(BaseModel):
     """Attachment model."""
 
+    model_config = ConfigDict(alias_generator=camelize, populate_by_name=True)
+
     original_rowid: int = Field(alias="originalROWID")
     guid: UUID
     uti: str
-    mime_type: str = Field(alias="mimeType")
-    transfer_name: str = Field(alias="transferName")
-    total_bytes: int = Field(alias="totalBytes")
+    mime_type: str
+    transfer_name: str
+    total_bytes: int
     height: int
     width: int
     metadata: AttachementMetadata
 
 
-class WebhookNewMessageData(BaseModel):
-    """Webhook data model."""
+class BaseWebhookMessageData(BaseModel):
+    """Base webhook data model."""
+
+    model_config = ConfigDict(alias_generator=camelize, populate_by_name=True)
 
     original_rowid: int = Field(alias="originalROWID")
     guid: UUID
     text: str
-    attributed_body: str | None = Field(alias="attributedBody")
+    attributed_body: str | None
     handle: Handle
-    handle_id: int = Field(alias="handleId")
-    other_handle: int = Field(alias="otherHandle")
+    handle_id: int
+    other_handle: int
     attachments: list[Attachememt]
     subject: str | None
     error: int
-    date_created: datetime = Field(alias="dateCreated")
-    date_read: datetime | None = Field(alias="dateRead")
-    date_delivered: datetime | None = Field(alias="dateDelivered")
-    is_from_me: bool = Field(alias="isFromMe")
-    has_dd_results: bool = Field(alias="hasDdResults")
-    is_archived: bool = Field(alias="isArchived")
-    item_type: int = Field(alias="itemType")
-    group_title: str | None = Field(alias="groupTitle")
-    group_action_type: int = Field(alias="groupActionType")
-    balloon_bundle_id: str | None = Field(alias="balloonBundleId")
-    associated_message_guid: str | None = Field(alias="associatedMessageGuid")
-    associated_message_type: str | None = Field(alias="associatedMessageType")
-    expressive_send_style_id: str | None = Field(alias="expressiveSendStyleId")
-    thread_originator_guid: str | None = Field(alias="threadOriginatorGuid")
-    has_payload_data: bool = Field(alias="hasPayloadData")
+    date_created: datetime
+    date_read: datetime | None
+    date_delivered: datetime | None
+    is_from_me: bool
+    has_dd_results: bool
+    is_archived: bool
+    item_type: int
+    group_title: str | None
+    group_action_type: int
+    balloon_bundle_id: str | None
+    associated_message_guid: str | None
+    associated_message_type: str | None
+    expressive_send_style_id: str | None
+    thread_originator_guid: str | None
+    has_payload_data: bool
+    message_summary_info: dict | None
+    payload_data: dict | None
+
+
+class WebhookNewMessageData(BaseWebhookMessageData):
+    """Webhook data model for new messages."""
+
     chats: list[Chat]
-    message_summary_info: dict | None = Field(alias="messageSummaryInfo")
-    payload_data: dict | None = Field(alias="payloadData")
 
 
 class WebhookNewMessage(BaseModel):
     """Webhook model for new messages."""
 
+    model_config = ConfigDict(alias_generator=camelize, populate_by_name=True)
+
     type: Literal["new-message"]
     data: WebhookNewMessageData
 
 
+class WebhookUpdatedMessage(BaseModel):
+    """Webhook model for new messages."""
+
+    model_config = ConfigDict(alias_generator=camelize, populate_by_name=True)
+
+    type: Literal["updated-message"]
+    data: BaseWebhookMessageData
+
+
 class WebhookTypingIndicatorData(BaseModel):
     """Webhook data model for typing indicators."""
+
+    model_config = ConfigDict(alias_generator=camelize, populate_by_name=True)
 
     display: bool
     guid: str
@@ -99,6 +128,8 @@ class WebhookTypingIndicatorData(BaseModel):
 
 class WebhookTypingIndicator(BaseModel):
     """Webhook model for typing indicators."""
+
+    model_config = ConfigDict(alias_generator=camelize, populate_by_name=True)
 
     type: Literal["typing-indicator"]
     data: WebhookTypingIndicatorData
